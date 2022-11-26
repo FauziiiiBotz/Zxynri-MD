399457079e79c672203addbaa5590cd123f25a3c
FauziiiiBotz <116927645+FauziiiiBotz@users.noreply.github.com>
2022-11-26 14:40:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,31 @@
 // RECODE BY ZIFABOTZ
 
 //=============『 Utama 』================== //
-global.owner = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.mods = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.prems = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
+global.owner = ['6287837703726'] //ganti pakai nomormu biar bot berfungsi
+global.mods = ['6287837703726'] //ganti pakai nomormu biar bot berfungsi
+global.prems = ['6287837703726'] //ganti pakai nomormu biar bot berfungsi
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ᴹᴿ᭄ DaniBotzOffcོ_1ঔৣ꧂'
-global.numberowner = '6281251549899' //ganti pakai nomor mu
-global.instagram = 'Gk Ad Bwang:v' //ganti pakai Instagram mu
-global.github = 'https://github.com/Bot-Mdnew' //ganti aja pakai githubmu
-global.tt = '"_"' //ganti aja pakai tiktok mu
-global.sc = 'https://youtu.be/lMcwJMp_0rM' //jngn ganti hargai creator
-global.ss = 'https://youtu.be/3m7tRD4RklM' //jngn ganti hargai creator
-global.sss = 'https://youtu.be/PzUH1ApMCig' //jngn ganti hargai creator
+global.nameowner = 'FauziGanz꧂'
+global.numberowner = '6287837703726' //ganti pakai nomor mu
+global.instagram = 'http://Instagram.com/lukman_lim11' //ganti pakai Instagram mu
+global.github = 'https://github.com/FauziSlebew' //ganti aja pakai githubmu
+global.tt = 'tiktok.com/@fauziiibeban__' //ganti aja pakai tiktok mu
+global.sc = 'wa.me/6287837703726' //jngn ganti hargai creator
+global.ss = 'Wa.me/6287837703726?text=Banh Minta Sc🤓🗿' //jngn ganti hargai creator
+global.sss = 'Wa.me/6287837703726?text=Banh Minta Sc🤓🗿' //jngn ganti hargai creator
 global.yt = 'https://youtube.com/channel/UCeUvY_fIVSAaQ1n5SAylI-A'
-global.dana = '081251549899' //ganti pakai nomor dana mu
-global.pulsa = '081352848936' //ganti pakai nomor pulsa mu
-global.gopay = '081352848936' //ganti pakai nomor Gopay mu
+global.dana = '087837703726' //ganti pakai nomor dana mu
+global.pulsa = '087837703726' //ganti pakai nomor pulsa mu
+global.gopay = '087837703726' //ganti pakai nomor Gopay mu
 
 //=============『 Info Bot 』=================//
-global.namebot = '❋ཻུ۪۪⸙Danibotz-Ofc⳹ ❋ཻུ۪۪' //ganti jadiin nama bot mu
-global.gc = 'https://chat.whatsapp.com/BiJs5Xl4oJA1Ama5BahUwd' //ganti pakai grup mu
+global.namebot = 'LimBotz-Md' //ganti jadiin nama bot mu
+global.gc = 'https://chat.whatsapp.com/HhhKiM5V54OFiDFqSxhEwr' //ganti pakai grup mu
 global.web = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = '"_"' //ganti pakai Instagram mu
+global.webm = 'http://Instagram.com/lukman_lim11' //ganti pakai Instagram mu
 global.saweria = '"_"' //bebas ganti aja pakai apa
-global.nomer = 'https://wa.me/6281251549899' //ganti jadi nomor mu
+global.nomer = 'https://wa.me/628315435675' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -42,12 +42,12 @@ global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini gant
 global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
 global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
 global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379e.jpg' //ganti pakai poto payment atau apa
-global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.lolkey = 'LimBotz-Md' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '❋ཻུ۪۪Danibotz-Ofc⳹ ❋ཻུ۪۪'
+global.wm = '❋ཻུ۪۪LimBotz-Ofc⳹ ❋ཻུ۪۪'
 global.watermark = wm
-global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜDani_ofcོ1꧅'
-global.wm3 = 'ᯤ Dani Offcial'
+global.wm2 = 'ᯤ creator by Fauzi꧅'
+global.wm3 = 'ᯤ Fauzi'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -57,8 +57,8 @@ global.eror = '_*🚨 Server Bot Sedang Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '⏳ Stiker sedang Proses'
-global.packname = 'DaniBotz-Ofc'
-global.author = '@Dani Official'
+global.packname = 'LimBotz-Md'
+global.author = '@axlfauzixy'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
